f7df1a9f4778c7c5e61d2e4006210f86e5630bdc
22050908-Meng Pei <2398696309@qq.com>
2023-01-29 09:57:05 +0800

    Already have plugins and configures

---
+++ init.vim
@@ -0,0 +1,11 @@
+set relativenumber
+set number
+ 
+set tabstop=2
+set shiftwidth=2
+set expandtab
+ 
+set completeopt=menu,menuone,noselect
+set nofoldenable
+
+lua require('plugins')
---
+++ lua/plugins.lua
@@ -0,0 +1,301 @@
+vim.g.loaded_newtrw = 1
+vim.g.loaded_newtrPlugin = 1
+vim.opt.termguicolors = true
+vim.g.mapleader = "<Space>"
+vim.notify = require("notify")
+ 
+vim.cmd[[colorscheme tokyonight-moon]]
+ 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+ 
+local packer_bootstrap = ensure_packer()
+ 
+require('packer').startup({function(use)
+  -- Packer can manage itself 
+  use 'wbthomason/packer.nvim'
+ 
+  -- lsp installer
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'neovim/nvim-lspconfig'
+ 
+  use 'nvim-lua/plenary.nvim'
+  use 'jose-elias-alvarez/null-ls.nvim'
+ 
+  -- Completion
+  use'hrsh7th/cmp-nvim-lsp'
+  use'hrsh7th/cmp-buffer'
+  use'hrsh7th/cmp-path'
+  use'hrsh7th/cmp-cmdline'
+  use'hrsh7th/nvim-cmp'
+  use'L3MON4D3/LuaSnip'
+  use'saadparwaiz1/cmp_luasnip'
+ 
+  -- Troubles
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+      }
+    end
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+    require('gitsigns').setup()
+    end
+  }
+ 
+  -- Terminal
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+  end}
+ 
+  -- Comment
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+  }
+ 
+  -- File Explorer
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+ 
+  -- Tree Sitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+ 
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+ 
+  -- Theme and StatusLine
+  use 'folke/tokyonight.nvim'
+  use 'nvim-lualine/lualine.nvim'
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+
+  -- Startup
+  use {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end
+  }
+
+  -- Which Key
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  -- Notify
+  use 'rcarriga/nvim-notify'
+ 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end,
+config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}
+})
+ 
+require('mason').setup()
+require('mason-lspconfig').setup()
+ 
+-- Completion Setting
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+ 
+local luasnip = require("luasnip")
+local cmp = require'cmp'
+ 
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-z>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+ 
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+ 
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+ 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+ 
+local lspconfig = require('lspconfig')
+ 
+local servers = { 'clangd' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+  }
+end
+ 
+-- NvimTreeConfig
+require('nvim-tree').setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+  git = {
+    ignore = false;
+  },
+})
+ 
+-- Tree Sitter Setting
+vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod     = 'expr'
+    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+  end
+})
+ 
+-- Status Line Setting
+require('lualine').setup {
+  options = {
+    theme = 'auto',
+    disabled_filetypes = {
+      statusline = {'NvimTree'},
+      winbar = {},
+    },
+    ignore_focus = {
+      'NvimTree',
+    },
+  }
+}
+require('bufferline').setup {
+  options = {
+    offsets = {
+      {
+        filetype = 'NvimTree',
+        text = 'File Explorer',
+        text_align = 'center',
+        separator = true
+      }
+    },
+    diagnostics = 'nvim_lsp',
+    diagnostics_update_in_insert = false,
+    numbers = 'ordinal'
+  }
+}
+ 
+-- null-ls Setting
+local null_ls = require("null-ls")
+ 
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.diagnostics.cpplint,
+        null_ls.builtins.completion.luasnip,
+    },
+})
