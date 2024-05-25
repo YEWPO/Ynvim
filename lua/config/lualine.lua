77612ddf44dc61673da1cf855d2b28538cd37882
22050908-Meng Pei <2398696309@qq.com>
2024-05-25 22:08:19 +0800

    feat: lualine ignore aerial

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,15 +1,16 @@
 require("lualine").setup {
   options = {
-    component_separators = {left = '', right = ''},
-    section_separators = {left = '', right = ''},
+    component_separators = {left = "", right = ""},
+    section_separators = {left = "", right = ""},
     theme = "tokyonight",
     disabled_filetypes = {
-      statusline = {'NvimTree', 'toggleterm'},
+      statusline = {"NvimTree", "toggleterm"},
       winbar = {},
     },
     ignore_focus = {
-      'NvimTree',
-      'toggleterm',
+      "NvimTree",
+      "toggleterm",
+      "aerial",
     },
   },
 }
