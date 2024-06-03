1ee7c2a4a99dcc5906626011d0f86a6179ee78de
22050908-Meng Pei <2398696309@qq.com>
2024-06-03 11:42:21 +0800

    fix: fix tab bug

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,5 +1,11 @@
 local cmp = require("cmp")
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local kind_icons = {
   Text = "",
   Method = "󰆧",
@@ -56,25 +62,35 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
+    ["<CR>"] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    }),
+    ["<Tab>"] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= "prompt" and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
       end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= "prompt" and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
       end
-    end, { "i", "s" }),
+    end,
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
--- lua/main.lua
+++ lua/main.lua
@@ -25,7 +25,10 @@ local plugins = {
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
-  "hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+  },
 
   {
     "nvim-tree/nvim-tree.lua",
