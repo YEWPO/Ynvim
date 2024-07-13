b0ceedd73579d34e1fabf4bb1fc7e4992d9de544
22050908-Meng Pei <2398696309@qq.com>
2024-07-13 14:51:09 +0800

    update: update keymapping

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,36 +1,26 @@
 vim.g.mapleader = " "
 
 local key_mappings = {
-  s = {
-    name = "+Save File Options",
-    s = {"<cmd>w<cr>", "Save File Only"},
-    q = {"<cmd>wq<cr>", "Save and Quit"},
-  },
-  q = {
-    name = "+Quit Options",
-    q = {"<cmd>q<cr>", "Quit"},
-    a = {"<cmd>qa<cr>", "Quit All"},
-    f = {"<cmd>q!<cr>", "Quit Force"},
-    F = {"<cmd>qa!<cr>", "Quit All Force"},
-  },
-  o = {"o<esc>k", "Create a new line below"},
-  O = {"O<esc>k", "Create a new line above"},
-  w = {
-    name = "+Windows Option",
-    s = {"<cmd>split<cr>", "Horizontal Split"},
-    v = {"<cmd>vsplit<cr>", "Vertical Split"},
-    w = {"<c-w><c-w>", "Switch Windows"},
-    l = {"<cmd>wincmd l<cr>", "Right Window"},
-    h = {"<cmd>wincmd h<cr>", "Left Window"},
-    k = {"<cmd>wincmd k<cr>", "Top Window"},
-    j = {"<cmd>wincmd j<cr>", "Button Window"},
-  },
-  c = {"\"+y", "Copy to Clipboard", mode = "v"},
-  i = {"\"+p", "Insert from Clipboard"},
+  { "<leader>s", group = "Save"},
+  { "<leader>ss", "<cmd>w<cr>", desc = "Save File Only"},
+  { "<leader>sq", "<cmd>wq<cr>", desc = "Save and Quit"},
+  { "<leader>q", group = "Quit"},
+  { "<leader>qq", "<cmd>q<cr>", desc = "Quit"},
+  { "<leader>qa", "<cmd>qa<cr>", desc = "Quit All"},
+  { "<leader>qf", "<cmd>q!<cr>", desc = "Quit Force"},
+  { "<leader>qF", "<cmd>qa!<cr>", desc = "Quit All Force"},
+  { "<leader>o", "o<esc>k", desc = "Create a new line below"},
+  { "<leader>O", "O<esc>j", desc = "Create a new line above"},
+  { "<leader>w", group = "Windows"},
+  { "<leader>ws", "<cmd>split<cr>", desc = "Horizontal Split"},
+  { "<leader>wv", "<cmd>vsplit<cr>", desc = "Vertical Split"},
+  { "<leader>ww", "<c-w><c-w>", desc = "Switch Windows"},
+  { "<leader>wl", "<cmd>wincmd l<cr>", desc = "Right Window"},
+  { "<leader>wh", "<cmd>wincmd h<cr>", desc = "Left Window"},
+  { "<leader>wk", "<cmd>wincmd k<cr>", desc = "Top Window"},
+  { "<leader>wj", "<cmd>wincmd j<cr>", desc = "Button Window"},
+  { "<leader>c", "\"+y", desc = "Copy to Clipboard", mode = "v"},
+  { "<leader>i", "\"+p", desc = "Insert from Clipboard"},
 }
 
-local opts = {
-  prefix = "<leader>",
-}
-
-require("which-key").register(key_mappings, opts)
+require("which-key").add(key_mappings)
