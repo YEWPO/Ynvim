fa0d878b0c9ab9f46545637125dacb7f18643dd8
Pei Meng <2398696309@qq.com>
2025-08-12 14:10:48 +0800

    update: treesitter-context

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -7,3 +7,10 @@ require("nvim-treesitter.configs").setup {
     enable = true,
   }
 }
+
+require("treesitter-context").setup {
+  enable = true,
+  max_lines = 0, -- No limit on the number of lines
+  trim_scope = "outer", -- Show context from outer scopes
+  min_window_height = 0, -- No minimum window height
+}
